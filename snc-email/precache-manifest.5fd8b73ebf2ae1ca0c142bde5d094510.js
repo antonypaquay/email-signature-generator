daf5db5aa02958342f8de2d7aba3fc4acc6f6be8
Antony Paquay <antony.paquay@gmail.com>
2020-09-08 17:53:24 +0200

    [UPD] fix some issues on outlook

---
+++ snc-email/precache-manifest.5fd8b73ebf2ae1ca0c142bde5d094510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2e2c7f0501fb4a839673a40037f77e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d1c7f9c4c3dcb2689f2",
+    "url": "/static/css/main.9a182d1c.chunk.css"
+  },
+  {
+    "revision": "e6e8de69f2ed999d2a81",
+    "url": "/static/js/2.e03f682a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e03f682a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1c7f9c4c3dcb2689f2",
+    "url": "/static/js/main.c8dfad5e.chunk.js"
+  },
+  {
+    "revision": "7b45c5d5a32cc9eb53a8",
+    "url": "/static/js/runtime-main.2652b058.js"
+  }
+]);
