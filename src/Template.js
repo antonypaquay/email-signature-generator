e4c99da6dd842a2555e765597f4fbfc211bf265f
Antony Paquay <antony.paquay@gmail.com>
2021-01-13 10:44:41 +0100

    [UPD] update email template

--- src/Template.js
+++ src/Template.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './index.scss';
-import Facebook from './assets/img/facebook.png';
-import Linkedin from './assets/img/linkedin.png';
 
 const Template = ({ firstName, lastName, position, phone }) => {
     return (
@@ -10,37 +8,37 @@ const Template = ({ firstName, lastName, position, phone }) => {
                 <tbody>
                     <tr>
                         <td colSpan="2">
-                            <img width="65" height="65" style={{ 'display': 'block', 'maxWidth': '65px' }} src="https://www.seekandcare.com/t_theme_seekandcare/static/src/img/logo-colors-square-64px@2x.png" alt="Logo SeekandCare" />
+                            <img width="65" height="65" style={{ 'display': 'block', 'maxWidth': '65px' }} src="https://www.seekandcare.com/theme_snc/static/src/img/emails/logo.png" alt="Logo SeekandCare" />
                         </td>
                         <td colSpan="8">
-                            <h1 style={{ 'fontSize': '20px', 'color': '#3C3C46', 'marginBottom': '8px' }}>{firstName} {lastName}<br /><span style={{ 'display': 'block', 'fontWeight': '500', 'fontSize': '16px', 'fontStyle': 'italic' }}>{position}</span></h1>
-                            <p style={{ 'margin': '4px 0', 'fontSize': '0.875rem' }}>
-                                Email: <a style={{ 'fontWeight': '500', 'fontSize': '14px', 'color': '#508BAF' }} href={`mailto:${firstName.toLowerCase()}.${lastName.toLowerCase()}@seekandcare.com`}>{firstName.toLowerCase()}.{lastName.toLowerCase()}@seekandcare.com</a>
+                            <h1 style={{ 'fontSize': '20px', 'color': '#283e76', 'marginBottom': '8px' }}>{firstName} {lastName}<br /><span style={{ 'display': 'block', 'fontWeight': '500', 'fontSize': '16px', 'fontStyle': 'italic' }}>{position}</span></h1>
+                            <p style={{ 'margin': '4px 0', 'fontSize': '0.875rem', 'color': '#808db0' }}>
+                                Email: <a style={{ 'fontWeight': '500', 'fontSize': '14px', 'color': '#3791eb' }} href={`mailto:${firstName.toLowerCase()}.${lastName.toLowerCase()}@seekandcare.com`}>{firstName.toLowerCase()}.{lastName.toLowerCase()}@seekandcare.com</a>
                             </p>
-                            <p style={{ 'margin': '4px 0', 'fontSize': '0.875rem' }}>
-                                Phone: <a style={{ 'fontWeight': '500', 'fontSize': '14px', 'color': '#508BAF' }} href={`${phone}`}>{phone}</a>
+                            <p style={{ 'margin': '4px 0', 'fontSize': '0.875rem', 'color': '#808db0' }}>
+                                Phone: <a style={{ 'fontWeight': '500', 'fontSize': '14px', 'color': '#3791eb' }} href={`${phone}`}>{phone}</a>
                             </p>
-                            <address style={{ 'fontSize': '14px', 'color': '#3C3C46', 'marginBottom': '4px' }}>
+                            <address style={{ 'fontSize': '14px', 'color': '#283e76', 'marginBottom': '4px' }}>
                                 Rue de l'industrie, 20 1400 Nivelles Belgique
                             </address>
                             <table>
                                 <tr>
-                                    <td colSpan="1" width="12" height="12" style={{ 'backgroundColor': '#C8DDE9', 'borderRadius': '20px' }}>
-                                        <a style={{ 'display': 'block', 'padding': '8px'}} href="https://www.facebook.com/seekandcarecom/">
-                                            <img width="12" height="12" style={{ 'display': 'block', 'maxWidth': '12px' }} src={Facebook} alt="Facebook" />
+                                    <td colSpan="1" width="10" height="10" style={{ 'backgroundColor': '#283e76', 'borderRadius': '20px' }}>
+                                        <a style={{ 'display': 'block', 'padding': '6px'}} href="https://www.facebook.com/seekandcarecom/">
+                                            <img width="10" height="10" style={{ 'display': 'block', 'maxWidth': '12px' }} src="https://www.seekandcare.com/theme_snc/static/src/img/emails/facebook.png" alt="Facebook" />
                                         </a>
                                     </td>
-                                    <td colSpan="1" width="12" height="12" style={{ 'backgroundColor': '#C8DDE9', 'borderRadius': '20px' }}>
-                                        <a style={{ 'display': 'block', 'padding': '8px'}} href="https://www.linkedin.com/company/seek-care/">
-                                            <img width="12" height="12" style={{ 'display': 'block', 'maxWidth': '12px' }} src={Linkedin} alt="Linkedin" />
+                                    <td colSpan="1" width="10" height="10" style={{ 'backgroundColor': '#283e76', 'borderRadius': '20px' }}>
+                                        <a style={{ 'display': 'block', 'padding': '6px'}} href="https://www.linkedin.com/company/seek-care/">
+                                            <img width="10" height="10" style={{ 'display': 'block', 'maxWidth': '12px' }} src="https://www.seekandcare.com/theme_snc/static/src/img/emails/linkedin.png" alt="Linkedin" />
                                         </a>
                                     </td>
                                     <td colSpan="6">
-                                        <a style={{ 'fontWeight': '500', 'fontSize': '14px', 'color': '#508BAF', 'marginLeft': '4px' }} href="https://seekandcare.com">https://seekandcare.com</a>
+                                        <a style={{ 'fontWeight': '500', 'fontSize': '14px', 'color': '#3791eb', 'marginLeft': '4px' }} href="https://www.seekandcare.com/">https://www.seekandcare.com/</a>
                                     </td>
                                 </tr>
                             </table>
-                            <small style={{ 'fontSize': '12px' }}>AP Solutions <span>VAT: BE0716802977</span></small>
+                            <small style={{ 'fontSize': '12px', 'color': '#808db0' }}>AP Solutions <span>VAT: BE0716802977</span></small>
                         </td>
                     </tr>
                 </tbody>
